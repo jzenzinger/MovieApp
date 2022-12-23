cd8e764e759e876b2f2696de1d56d97264da07bb
jzenzinger <juris.zen@seznam.cz>
2022-12-23 21:13:06 +0100

    Axios call - NEED WORK

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
-import React from 'react';
-import {Text} from 'react-native';
-import {
-    NativeBaseProvider,
-    Box,
-    VStack
-} from "native-base";
-
+import {useEffect, useState} from 'react';
+import {Text, FlatList, ActivityIndicator} from 'react-native';
+import axios from "axios";
+import { NativeBaseProvider, Box, VStack } from "native-base";
 // Components imports
 import Footer from "./views/Footer";
 
+const API = process.env.API_URL;
+
 const App = () => {
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+
+    const getMoviesFromAPI = async () => {
+        try {
+            const url = `${API}&t=mission`;
+            const response = await axios.get(url);
+            setData(response);
+        } catch (error) {
+            console.error(error);
+            console.log(API);
+        // }
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        getMoviesFromAPI();
+    }, []);
+
     return (
         <NativeBaseProvider width="100%">
             <VStack height="90%">
-                <Text>
-                    Content
-                </Text>
+                {isLoading ? <ActivityIndicator/> : (
+                    <FlatList
+                        data={data}
+                        keyExtractor={({ id }, index) => id}
+                        renderItem={({ item }) => (
+                            <Text>{item.title}, {item.year}, {item.genre}</Text>
+                        )}
+                    />
+                )}
             </VStack>
             <Box safeAreaTop width="95%" alignSelf="center">
                 <Footer/>
