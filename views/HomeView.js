6c7090f4ff095f717465214f236b8c5f65e76dc7
jzenzinger <juris.zen@seznam.cz>
2023-01-23 17:50:37 +0100

    Home View Text

--- views/HomeView.js
+++ views/HomeView.js
@@ -1,18 +1,41 @@
-import { Center } from "native-base";
+import {Center, Text, Box} from "native-base";
+import {StyleSheet} from "react-native";
 
 
 const HomeView = () => {
     return (
-        <Center w="100%" h="30%" bg="indigo.600" elevation={10} _text={{
-            textAlign: 'center',
-            alignItems: 'center',
-            fontWeight: '800',
-            fontSize: '4xl',
-            color: 'white'
-        }}>
-            Welcome back!
-        </Center>
+        <Box height="100%" width="100%">
+            <Center w="100%" h="40%" bg="indigo.700" elevation={20}>
+                <Text fontSize="5xl" style={styles.heading}>Welcome back!</Text>
+                <Text fontSize="xl" marginTop="5" style={styles.heading}>Ready to search favourites {"\n"}movies/series again?</Text>
+            </Center>
+            <Box marginLeft="10" marginRight="10">
+                <Text fontSize="md" marginTop="15%" style={styles.text}>This application was created with React-Native
+                    and UI library called Native-Base.</Text>
+                <Text fontSize="md" marginTop="2" style={styles.text}>Application is supposed to be for passing school
+                    project, exam called Mobile Technology for Android.</Text>
+                <Text fontSize="md" marginTop="2" style={styles.text}>Tech Stack:</Text>
+                <Text fontSize="md" marginLeft="10" style={styles.text}>- React-Native</Text>
+                <Text fontSize="md" marginLeft="10" style={styles.text}>- Native-Base</Text>
+                <Text fontSize="md" marginLeft="10" style={styles.text}>- OMDB API</Text>
+            </Box>
+        </Box>
     );
 }
+
+const styles = StyleSheet.create({
+    heading: {
+        textAlign: "center",
+        alignItems: "center",
+        fontWeight: "800",
+        color: "white"
+    },
+    text: {
+        textAlign: "left",
+        alignItems: "center",
+        fontWeight: "400",
+        color: "black"
+    }
+});
 
 export default HomeView;
